956337417b00d17cc761e30a871ac46cc653290d
Aman-kushwaha58 <amankushwaha3328@gmail.com>
2025-02-17 22:31:09 +0530

    Add files via upload

---
+++ average.py
@@ -0,0 +1,7 @@
+#first = int(input("enter the firstnum: "))
+#second = float(input("enter the secondmum: "))
+#print("average= ", (first+second)/2)
+
+a=int(input("enetr the value: "))
+b=int(input("eneter the value"))
+print(a<=b)
