639bf7b6f1bcc22b20b80b67c2a6dc8d5678c690
Aman-kushwaha58 <amankushwaha3328@gmail.com>
2025-02-20 22:36:23 +0530

    Add files via upload

---
+++ lecture_python.py
@@ -0,0 +1,60 @@
+# marks1 = 94.4
+# marks2 = 87.5
+# marks3 = 97.3
+# marks4 = 67.4
+
+# marks = [94.4, 87.5, 97.3, 67.4]
+# print(marks)
+# print(type(marks))
+# print(len(marks))
+# print(marks[0])
+
+# question 2
+# student = ["Aman", 97.5, 17, "kalaiya"]
+# print(student)
+
+# str = "hello" (fault code)
+# print(student[0])
+# student[0] = "Aman"
+# print(student)
+
+# question 3
+# marks = [94.4, 87.5, 97.3, 67.4]
+# print(marks [1:4])
+
+# que 5 add any number in list
+
+# list = [2, 1, 3]
+# list.append(4)
+# print(list)
+
+#que 6 sort the number
+# list = [2, 1, 3]
+# list.append(4)
+# print(list.sort())
+# print(list)
+  
+#   que 7 reverse
+# list = [2, 1, 3]
+# list.append(4)
+# print(list.sort(reverse=True))
+# print(list)
+
+# que 8 sort also possible in string in descending
+# list = ["bannana", "apple", "litche"]
+# list.append("randi")
+# print(list.sort(reverse=True))
+# print(list)
+
+# que 9 sort also possiblle in string in ascending
+# list = ["bannana", "apple", "litche"]
+# list.append("randi")
+# print(list.sort())
+# print(list)
+
+# que 10 insert num
+
+list = [2, 1, 3]
+list.insert(1, 5)
+print(list)
+# list.remove() or list.pop()
---
+++ len.py
@@ -0,0 +1,47 @@
+#name = input("enter your name :")
+#print("length of your name is", len(name))
+# age = 34
+# if(age>=18):
+#     print("can vote & apply for license")
+#     print("can drive")
+
+# question3
+# light = "red"
+
+# if(light == "red"):
+#     print("stop")
+# elif(light == "green"):
+#     print("go")
+# elif(light == "yellow"):
+#     print("look")
+
+# question4
+
+# marks = int(input("enter student marks: "))
+
+# if(marks >= 90):
+#     grade = "A"
+# elif(marks >= 80):
+#     grade = "B"
+# elif(marks >= 70):
+#     grade = "c"
+# else:
+#     grade = "D"
+#     print("grade of the student ->",grade)
+
+# Question 5
+#  tuples
+
+# tup = (2, 1, 3, 1)
+# print(tup[0])
+# print(tup[2])
+# print(type(tup)) #type of tup
+
+# tuple method
+# if you thing to find element one the 
+tup = (1, 2, 3, 4)
+print(tup.index(2))
+
+# for count function 
+tup = (1, 2, 3, 4)
+print(tup.count(2))
